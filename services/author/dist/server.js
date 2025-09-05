bed5a943939ce3aba1799d290395cf26beb4739f
Arpit Kairati <arpitkairati54@gmail.com>
2025-09-05 11:31:20 +0530

    feat(author-service): initialize author service with database setup and basic server configuration
    - Added package.json with necessary dependencies and scripts for development and production.
    - Implemented server.ts to set up Express server and initialize database with tables for blogs, comments, and saved blogs.
    - Created db.ts for database connection using Neon serverless.
    - Configured TypeScript with tsconfig.json for proper compilation and type checking.

---
+++ services/author/dist/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./utils/db.js";
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+async function initDB() {
+    try {
+        await sql `
+        CREATE TABLE IF NOT EXISTS blogs(
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description VARCHAR(255) NOT NULL,
+            blogcontent TEXT NOT NULL,
+            image VARCHAR(255) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            author VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`;
+        await sql `
+        CREATE TABLE IF NOT EXISTS comments(
+            id SERIAL PRIMARY KEY,
+            COMMENT VARCHAR(255) NOT NULL,
+            userid VARCHAR(255) NOT NULL,
+            username VARCHAR(255) NOT NULL,
+            blogid VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`;
+        await sql `
+        CREATE TABLE IF NOT EXISTS savedblogs(
+            id SERIAL PRIMARY KEY,
+            userid VARCHAR(255) NOT NULL,
+            blogid VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`;
+        console.log('Database created successfully');
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+initDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+});
+//# sourceMappingURL=server.js.map
