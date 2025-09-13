2c7504f32cf299b1d2876a37a9336e9c2c2d72c3
Arpit Kairati <arpitkairati54@gmail.com>
2025-09-13 14:41:11 +0530

    feat: implement comment fetching and deletion functionality; enhance comment display with user information

--- frontend/src/app/blog/[id]/page.tsx
+++ frontend/src/app/blog/[id]/page.tsx
@@ -7,12 +7,20 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Blog, blog_service, User, userAppData } from '@/context/AppContext'
 import axios from 'axios';
-import { Bookmark, Edit, Trash2Icon } from 'lucide-react';
+import { Bookmark, Edit, Trash2, Trash2Icon, User2 } from 'lucide-react';
 import Link from 'next/link';
 import { useParams, useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import Cookies from 'js-cookie';
 import toast from 'react-hot-toast';
+
+interface Comment {
+  id: string;
+  userid: string;
+  comment: string;
+  created_at: string;
+  username: string;
+}
 
 const BlogPage = () => {
 
@@ -23,32 +31,65 @@ const BlogPage = () => {
     const[author,setAuthor]=useState<User | null>(null);
     const[loading,setLoading]=useState<boolean>(false);
     const[comment,setComment]=useState<string>("");
-    const[comments,setComments]=useState<Array<{_id:string,comment:string,author:string,blog:string,created_at:string}>>([]);
+    const[comments,setComments]=useState<Comment[]>([]);
+
+
+async function fetchComment() {
+  try {
+    setLoading(true);
+    const { data } = await axios.get(`${blog_service}/api/v1/comment/${id}`);
+    const commentsData = (data as { comments: Comment[] }).comments; 
+    setComments(commentsData);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    setLoading(false);
+  }
+}
+  useEffect(() => {
+    fetchComment();
+  }, [id]);
 
 async function fetchSingleBlog() {
-  try {
-        setLoading(true);
-        const { data } = await axios.get(`${blog_service}/api/v1/blog/${id}`);
-        const response = data as { blog: Blog; author: User }; 
-        setBlog(response.blog);
-        setAuthor(response.author);
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`${blog_service}/api/v1/blog/${id}`);
+      const response = data as { blog: Blog; author: User }; 
+    setBlog(response.blog);
+    setAuthor(response.author);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     } finally {
-        setLoading(false);
+      setLoading(false);
     }
-}
+  }
+
+const deleteComment = async (id: string) => {
+  if (confirm("Are you sure you want to delete this comment")) {
+    try {
+      setLoading(true);
+      const token = Cookies.get("token");
+      const { data } = await axios.delete(
+        `${blog_service}/api/v1/comment/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const response = data as { message: string }; 
+      toast.success(response.message);
+      fetchComment();
+    } catch (error) {
+      toast.error("Problem while deleting comment");
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+};
+
     
-
-    useEffect(()=>{
-        fetchSingleBlog();
-    },[id])
-
-    if(!blog || !author){
-        return <Loading></Loading>
-    }
-
-
     async function addComment(){
       try {
         setLoading(true);
@@ -60,11 +101,22 @@ async function fetchSingleBlog() {
         }); 
         toast.success("Comment added successfully");
         setComment("");
+        fetchComment();
       } catch (error) {
         toast.error("Something went wrong");
       }finally{
         setLoading(false);
       }
+    }
+
+
+
+    useEffect(()=>{
+        fetchSingleBlog();
+    },[id])
+
+    if(!blog || !author){
+        return <Loading></Loading>
     }
   return (
     <div className='max-w-4xl mx-auto p-6 space-y-6'>
@@ -112,6 +164,36 @@ async function fetchSingleBlog() {
           </CardContent>
         </Card>
       }
+      <Card>
+        <CardHeader>
+          <h3 className="text-lg font-medium">All Comments</h3>
+        </CardHeader>
+        <CardContent>
+           {
+            comments && comments.length > 0 ? comments.map((e,i)=>{
+              return <div key={i} className='border-b py-2 flex items-center gap-3'>
+                <div>
+                  <p className="font-semibold flex items-center gap-1"><span className="user border border-gray-600 rounded-full p-1">
+                    <User2></User2></span>{e.username}</p>
+                    <p>{e.comment}</p>
+                    <p className="text-xs text-gray-500">
+                    {new Date(e.created_at).toLocaleString()}</p>
+                </div>
+                    {e.userid === user?._id && (
+                    <Button
+                    className='cursor-pointer ml-auto'
+                      onClick={() => deleteComment(e.id)}
+                      variant={"destructive"}
+                      disabled={loading}
+                    >
+                      <Trash2 />
+                    </Button>
+                  )}
+              </div>
+            }) : <p>No comments yet..</p>
+           }
+        </CardContent>
+      </Card>
     </div>
   )
 }
